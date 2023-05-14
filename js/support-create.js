2d6f554e98a2e28b90ea49de86644e54187a9492
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-14 12:25:40 +0300

    add brantch

--- js/support-create.js
+++ js/support-create.js
@@ -1,7 +1,7 @@
-import { supportArr } from './support-arr';
+import { supportArr } from "./support-arr";
 
-const supportItemsEl = document.querySelector('.support-list');
-const makeSupporItems = itemsEl => {
+const supportItemsEl = document.querySelector(".support-list");
+const makeSupporItems = (itemsEl) => {
   const { title, url, img } = itemsEl;
   return `<li class="support-item">
   <a class="support-link" href=${url}>
@@ -14,33 +14,5 @@ const makeSupporItems = itemsEl => {
   </a>
 </li>`;
 };
-const itemsImages = supportArr.map(makeSupporItems).join('');
-supportItemsEl.insertAdjacentHTML('beforeend', itemsImages);
-
-// import svgLinkdUrl from '../images/linkedin.svg';
-// <a
-//   href="https://www.linkedin.com/in/nataliia-valko-951501212/"
-//   target="_blank"
-//   class="team-linkd"
-// >
-//   <svg width="24" height="24">
-//     <use href="${svgLinkdUrl}#icon-linkedin-svg"></use>
-//   </svg>
-// </a>;
-
-// <button class="btn-user-in" type="button">
-//   <span class="btn-icn-wrap">
-//     <img
-//       srcset="./images/user-@1x.png 1x, ./images/user-@2x.png 2x"
-//       src="./images/user-@1x.png"
-//       alt="user avatar"
-//       loading="lazy"
-//     />
-//   </span>
-//   Stephen
-//   <span class="btn-icn-wrap">
-//     <svg width="23" height="26">
-//       <use href="./images/icons.svg#caret-down"></use>
-//     </svg>
-//   </span>
-// </button>;
+const itemsImages = supportArr.map(makeSupporItems).join("");
+supportItemsEl.insertAdjacentHTML("beforeend", itemsImages);
