c0eeb8b94db47187e9eb65ac601ed523b5493699
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-14 14:07:04 +0300

    upd 2

--- js/support-create.js
+++ js/support-create.js
@@ -1,7 +1,7 @@
-import { supportArr } from "./support-arr";
+import { supportArr } from './support-arr';
 
-const supportItemsEl = document.querySelector(".support-list");
-const makeSupporItems = (itemsEl) => {
+const supportItemsEl = document.querySelector('.support-list');
+const makeSupporItems = itemsEl => {
   const { title, url, img } = itemsEl;
   return `<li class="support-item">
   <a class="support-link" href=${url}>
@@ -14,5 +14,22 @@ const makeSupporItems = (itemsEl) => {
   </a>
 </li>`;
 };
-const itemsImages = supportArr.map(makeSupporItems).join("");
-supportItemsEl.insertAdjacentHTML("beforeend", itemsImages);
+const itemsImages = supportArr.map(makeSupporItems).join('');
+supportItemsEl.insertAdjacentHTML('beforeend', itemsImages);
+
+// <button class="btn-user-in" type="button">
+//   <span class="btn-icn-wrap">
+//     <img
+//       srcset="./images/user-@1x.png 1x, ./images/user-@2x.png 2x"
+//       src="./images/user-@1x.png"
+//       alt="user avatar"
+//       loading="lazy"
+//     />
+//   </span>
+//   Stephen
+//   <span class="btn-icn-wrap">
+//     <svg width="23" height="26">
+//       <use href="./images/icons.svg#caret-down"></use>
+//     </svg>
+//   </span>
+// </button>;
